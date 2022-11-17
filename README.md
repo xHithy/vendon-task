b176a5d1b48fb2e95a0290af39f7b24a9a69093c
Hithy <hithyisgod@gmail.com>
2022-11-18 01:08:33 +0200

    Installation and configuration
    Added an installation and configuration guide

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# vendon-task
- This is a Vendon task for practicants
+# Vendon Task
+This is a simple test/quiz website.
+## Required
+**PHP version**: 7.4.21
+**mySQL version**: 5.7.34
+## Installation
+- Download the source-code. 
+- Place the folder **as the root directory** of your server.
+- Upload the SQL dump file to your phpMyAdmin
+- Open the example-config.php
+- Replace the temporary placeholders (example: **YOUR_DATABASE_HOST**, **YOUR_DATABASE_USERNAME**) with your database credentials and your websites URL to this folder
+- After changing the placeholders, rename the **example-config.php** file to **config.php**
+- Open your website and start the test!
+
+**Detailed instructions for every configuration step are inside the __example-config.php__ file!**
+
--- app/config/example-config.php
+++ app/config/example-config.php
@@ -1,14 +1,23 @@
 <?php
     /*
-    * ! IMPORTANT !
-    * Rename the file to config.php
+    * IMPORTANT
+    * After editing the config rename the file to config.php
     */
 
-    define("DB_HOST", "YOUR_HOST");
-    define("DB_USER", "YOUR_DATABASE_USERNAME");
-    define("DB_PASS", "YOUR_DATABASE_PASSWORD");
-    define("DB_NAME", "YOUR_DATABASE_NAME");
+    define("DB_HOST", "YOUR_DATABASE_HOST"); // Example: localhost:8889
+    define("DB_USER", "YOUR_DATABASE_USERNAME"); // Example: test
+    define("DB_PASS", "YOUR_DATABASE_PASSWORD"); // Example: test123
+    define("DB_NAME", "vendon-task"); // If you didn't rename the SQL dump file the name should be vendon-task
     
+
+    // This shouldn't be changed!
     define("APPROOT", dirname(dirname(__FILE__)));
-    define("URLROOT", "YOUR_ROOT_URL");
+    
+    /*
+    * If the sourcecode folder is the root directory, then the URL root should just be the domain;
+    * Example http://localhost:8888/
+    * ! The slash at the end is important !
+    */
+    define("URLROOT", "YOUR_ROOT_URL"); 
+
     define("SITENAME", "Vendon Task");
